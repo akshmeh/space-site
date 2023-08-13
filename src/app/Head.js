4e619d3b693291a7a813c64ecc0901ca26ec35e9
akshmeh <rishimehta257428@gmail.com>
2023-08-13 13:39:29 +0530

    check

--- src/app/Head.js
+++ src/app/Head.js
@@ -5,7 +5,7 @@ export default function Head({SiteName}) {
     <title>{SiteName}</title>
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
 <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Bellefair&display=swap" rel="stylesheet"></link>
-    <link rel="icon" href="./assets/favicon-32x32.png" />
+    <link rel="icon" href="/assets/favicon-32x32.png" />
     <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
     </head>
